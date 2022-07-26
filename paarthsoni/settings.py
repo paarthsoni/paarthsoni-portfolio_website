1bb7484fba4065df1c6790299f8257cd11512632
paarthsoni <paarth2812@gmail.com>
2022-07-27 00:35:00 +0530

    deployment change

--- paarthsoni/settings.py
+++ paarthsoni/settings.py
@@ -14,6 +14,8 @@ from django.contrib.messages import constants as messages
 import os
 from pathlib import Path
 import django_heroku
+import dj_database_url
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^=5$q)f%l^dk&)6mz@140gfw_f+u#mvk5fx(1uf%06m&cw^8-+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'paarthsoni.herokuapp.com']
 MESSAGE_TAGS = {
@@ -135,6 +137,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
